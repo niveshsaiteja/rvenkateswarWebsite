28f7db6f5796b170550835433f823f99d8239e45
niveshsaiteja <107808288+niveshsaiteja@users.noreply.github.com>
2023-08-03 18:14:31 +0530

    changes made

--- script.js
+++ script.js
@@ -63,15 +63,17 @@ window.addEventListener('resize', function () {
 	}
 })
 
-
+let cardContainer = document.getElementById("cardContainer");
 
 const switchMode = document.getElementById('switch-mode');
 
 switchMode.addEventListener('change', function () {
 	if(this.checked) {
 		document.body.classList.add('dark');
+		cardContainer.classList.add("white");
 	} else {
 		document.body.classList.remove('dark');
+		cardContainer.classList.remove("white");
 	}
 })
 
@@ -152,3 +154,87 @@ teamSideBar.onclick = function(){
 	companies.classList.add("d-none");
 	companies.classList.remove("d-block");
 };
+
+
+
+let data = [
+    {
+      "img":"assets/images/featured-1.png",
+      "heading":"The meet among the students for a placement",
+      "date":"02-05-2023",
+      "description": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit! Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit! Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit!  Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde."
+    },
+    {
+      "img":"assets/images/featured-1.png",
+      "heading":"The meet among the students for a placement",
+      "date":"02-05-2023",
+      "description": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit! Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit! Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit!  Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde."
+    },
+    {
+      "img":"assets/images/featured-1.png",
+      "heading":"The meet among the students for a placement",
+      "date":"02-05-2023",
+      "description": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit! Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit! Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit!  Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde."
+    },
+    {
+      "img":"assets/images/featured-1.png",
+      "heading":"The meet among the students for a placement",
+      "date":"02-05-2023",
+      "description": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit! Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit! Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit!  Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde."
+    },
+    {
+      "img":"assets/images/featured-1.png",
+      "heading":"The meet among the students for a placement",
+      "date":"02-05-2023",
+      "description": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit! Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit! Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit!  Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde."
+    },
+    {
+      "img":"assets/images/featured-1.png",
+      "heading":"The meet among the students for a placement",
+      "date":"02-05-2023",
+      "description": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit! Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit! Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, in? In quia nulla fuga rerum natus similique praesentium deserunt sit!  Officia maiores ullam cupiditate laborum vel. Adipisci odit ab unde."
+    }
+  ];
+
+
+
+for (let responce of data){
+  let CardDiv = document.createElement("div");
+  CardDiv.setAttribute("class","eve-card");
+  cardContainer.appendChild(CardDiv);
+
+  let imgDiv = document.createElement("div");
+  imgDiv.setAttribute("class","event-img-div");
+  CardDiv.appendChild(imgDiv);
+
+  let Img = document.createElement("img");
+  Img.src = responce.img;
+  Img.setAttribute("class","eve-img");
+  imgDiv.appendChild(Img);
+
+  let contentDiv = document.createElement("div");
+  contentDiv.setAttribute("class","eve-content");
+  CardDiv.appendChild(contentDiv);
+
+  let Heading = document.createElement("h1");
+  Heading.setAttribute("class","eve-heading");
+  Heading.textContent = responce.heading;
+  contentDiv.appendChild(Heading);
+
+  let span1 = document.createElement("span");
+  span1.setAttribute("class","eve-span");
+  contentDiv.appendChild(span1);
+
+  let icon = document.createElement("i");
+  icon.setAttribute("class","fa-solid fa-calendar-days eve-icon");
+  span1.appendChild(icon);
+
+  let innerspan = document.createElement("span");
+  innerspan.textContent = responce.date;
+  span1.appendChild(innerspan);
+
+  let DescriptionPara = document.createElement("para");
+  DescriptionPara.setAttribute("class","eve-description");
+  DescriptionPara.textContent = responce.description;
+  contentDiv.appendChild(DescriptionPara);
+}
